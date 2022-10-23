e2e6cff000a2561b088a51cee14f1d439cb09e4c
AshwatejHanchanale <ashwatejhanchanale@gmail.com>
2022-10-23 21:32:24 +0530

    adding data

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ this is a book record management API Backend for the management of records and b
 #routes and Endpoints
 
 ## /users
-post: create a new user
-get: get all list of users
+post: create a new user ✔
+get: get all list of users ✔
 
 ## /users/{id}
-GET: get a user by id
+GET: get a user by id  ✔
 PUT: update a user by id
 DELETE: delete a user by id (check if he/she still has an issued book)(IS there any fine to be paid).
 
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+//json data import
+const { users } = require("./data/users.json");
+
 const app = express();
 
 const port = 8081;
@@ -11,6 +14,76 @@ app.get("/", (req, res) => {
         message: "Server is running",
     });
 
+});
+
+/*
+*route : /users
+*Method: GET
+*Description: Get all users
+*Access : Public
+*Parameters: None
+*/
+
+app.get("/users", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: users,
+    });
+});
+
+/*
+*route : /users/id
+*Method: GET
+*Description: Get single user by id
+*Access : Public
+*Parameters: id
+*/
+app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const user = users.find((each) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "user not found",
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        message: user,
+    });
+});
+
+/*
+*route : /users
+*Method: POST
+*Description: create new user
+*Access : Public
+*Parameters: None
+*/
+app.post("/users", (req, res) => {
+    const { id, name, surname, email, subscriptionType, subscriptionDate } = req.body;
+
+    const user = users.find((each) => each.id === id);
+
+    if (user) {
+        return res.status(404).json({
+            success: false,
+            message: "This exist with this id",
+        });
+    }
+
+    users.push({
+        id,
+        name,
+        surname,
+        email,
+        subscriptionType,
+        subscriptionDate,
+    });
+    return res.status(200).json({
+        success: true,
+        data: users,
+    });
 });
 
 app.get("*", (req, res) => {
